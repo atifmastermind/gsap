a8ad39c3cc45047f941b3826d66a362178224cd3
Muhammad Atif <Atifmastermind12@gmail.com>
2024-07-11 15:52:49 +0500

    scrolltrigger

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // gsap.to(".box.green", { rotation: 360, x: 100, duration: 1, delay: 1.5 });
 // gsap.to(".box.blue", { rotation: 360, x: 100, duration: 2, delay: 2.5, backgroundColor: "red" });
 
-// using timeline
+// ! using timeline
 // let tl = gsap.timeline();
 
 // tl.to(".box.green", {
@@ -27,25 +27,45 @@
 //   delay: 1.5,
 // });
 
-let tl = gsap.timeline();
+// let tl = gsap.timeline();
 
-tl.from("h2", {
-  y: -20,
-  opacity: 0,
-  duration: 1,
-  delay: 0.5,
-});
-tl.from("li", {
-  y: -20,
-  opacity: 0,
-  duration: 1,
-  delay: 0.5,
-  stagger: 0.3,
-});
-tl.from("h1", {
-  y: -20,
-  opacity: 0,
-  duration: 1,
-  delay: 0.5,
-  scale: 2,
-});
+// tl.from("h2", {
+//   y: -20,
+//   opacity: 0,
+//   duration: 1,
+//   delay: 0.5,
+// });
+// tl.from("li", {
+//   y: -20,
+//   opacity: 0,
+//   duration: 1,
+//   delay: 0.5,
+//   stagger: 0.3,
+// });
+// tl.from("h1", {
+//   y: -20,
+//   opacity: 0,
+//   duration: 1,
+//   delay: 0.5,
+//   scale: 2,
+// });
+
+// ! scrollTrigger
+
+// gsap.from(".green .box", {
+//   scale: 0,
+//   opacity: 0,
+//   // delay: 1,
+//   duration: 3,
+//   rotate: 360,
+//   scrollTrigger: {
+//     trigger: "#second .box",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 60%",
+//     end: "top 30%",
+//     scrub: 2,
+//   },
+// });
+
+// pin
